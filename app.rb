cc5fab2d50116dc377b7393321e0abfa919561a4
disne_y <bold.disney@gmail.com>
2016-12-13 20:01:51 +0300

    Add views, add post&comments form, save all info to DB

--- app.rb
+++ app.rb
@@ -2,7 +2,56 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
-get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+set :database, "sqlite3:hipstozorium.db"
+
+class Post < ActiveRecord::Base 
+	has_many :comments
 end
+
+class Comment < ActiveRecord::Base
+	belongs_to :post
+end
+
+before do
+	@posts = Post.order('updated_at')
+	@posts = Comment.all
+
+end
+
+get '/' do	
+	@posts = Post.order "created_at DESC"
+	erb :index
+end
+
+get '/new' do
+	erb	:new
+end
+
+post '/new' do
+	p = Post.new params[:post]
+	p.save
+
+	erb 'Ваш пост записан!'
+end
+
+get '/post/:id' do
+	@post = Post.find(params[:id])
+	@comments = Post.find(params[:id]).comments.reorder('created_at')
+	erb :post
+end
+
+post '/post/:id' do
+	@post = Post.find(params[:id])
+
+	new_comment = Comment.new params[:comment]
+	new_comment.post_id = @post.id
+	new_comment.save
+	
+	erb 'Комментарий добавлен!'
+	#erb :post
+end
+
+
+
